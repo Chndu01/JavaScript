e0f3a03f5189e52731be2a2aaac3f6a331ebbd0a
Chndu01 <contactchandrakantt@gmail.com>
2025-03-12 23:31:31 +0530

    New Commit

---
+++ 2_data_types.js
@@ -0,0 +1,25 @@
+"use strict";  // treat all JS code as newer version.
+
+// alert("Hello");  ---  ReferenceError: alert is not defined.  we are using nodejs, not browser.
+
+// Data Types
+
+
+// String
+// Number
+// Boolean
+// BigInt
+// Undefined
+// null    Representation of intentional empty value
+// symbol  Unique
+// object 
+
+let name = "Chandrakant";   
+let age = 21;   
+let isLoggedIn = false; 
+
+console.log(typeof age);  // number 
+console.log(typeof null);  // object
+console.log(typeof undefined);  // undefined
+
+
