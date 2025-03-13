7cb357751be4b3db6246f84afe0d9f40f65c2ed0
Chndu01 <contactchandrakantt@gmail.com>
2025-03-13 11:07:54 +0530

    conversion of data types

---
+++ 3_conversionOperations.js
@@ -0,0 +1,34 @@
+// Conversion and Operations
+
+// let score = false;
+// let score = undefined;
+// let score = null;
+// let score = "33";
+// let score = "33abc";
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score);
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);    // Not a Number  NaN
+
+// "33" => 33
+// "33abc" => NaN
+// true  => 1,  false  => 0
+
+
+// let isLoggedIn = "Chandu"; 
+// let isLoggedIn = "";
+// let isLoggedIn = 1;
+// let isLoggedIn = 0;
+
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 33;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// Truthy and Falsy Values
