a98ad9fa08d072998e2c781eb85ecc35540c670c
Chndu01 <contactchandrakantt@gmail.com>
2025-03-13 11:57:25 +0530

    Comparison

---
+++ 4.js
@@ -0,0 +1,29 @@
+// Comparison
+
+console.log( 2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log( 2 == 1);
+console.log( 2 != 2);
+
+console.log("2" > 1);
+console.log( "02" > 1);
+
+/*  
+***** Avoid this type of comparison *****
+
+console.log(null > 0);
+console.log( null == 0);
+// console.log( null == 0);
+console.log( null >= 0);
+
+console.log(undefined == 0);
+
+*/
+
+// ===  strict check  value and data types check
+
+console.log("2" === 2);
+console.log("2" == 2);
+
+
