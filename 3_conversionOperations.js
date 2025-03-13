8327c7384bd8d1ed153952cd140bf4a15f632be6
Chndu01 <contactchandrakantt@gmail.com>
2025-03-13 11:44:26 +0530

    Conversion and Operations

--- 3_conversionOperations.js
+++ 3_conversionOperations.js
@@ -26,9 +26,47 @@
 // let booleanIsLoggedIn = Boolean(isLoggedIn);
 // console.log(booleanIsLoggedIn);
 
-let someNumber = 33;
-let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33;
+// let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 // Truthy and Falsy Values
+
+// **********************************  Operations   *************************** // 
+
+// let value = 3;
+// let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+
+// let str1 = "Hello";
+// let str2 = " Chandrakant";
+// let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log(1 + Number("2"));
+// console.log("2" + 2);  // if string comes first then it will treat all as strings.
+// console.log(1 + "2");
+// console.log( "2" + 2 + 2);
+// console.log(1 + 1 + "2");  // if number comes first then it will treat all as numbers.
+
+// console.log((3 + 4) * 5 % 3);
+
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1,num2, num3 
+// num1 = num2 = num3 = 4
+
+let gameCounter = 100;
+gameCounter++;  // 101
+++gameCounter;  // 102
+console.log(gameCounter);
