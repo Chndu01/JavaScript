82b54e2cf06230f839b164e7ff785e9d0e692b8a
Chndu01 <contactchandrakantt@gmail.com>
2025-03-15 20:04:28 +0530

    02_Basics

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,50 @@
+// array
+
+const myArray = [1,2,3,4,5,6,7,8,9,10];
+const myArry1 = new Array(2,3,4,5,44,3,33,333,22);
+const marverlHeroes = ["IronMan","CaptainAmerica"];
+// console.log(myArry1[1]);
+
+// Array Methods
+
+// myArray.push(3333);
+// console.log(myArray);
+
+// myArry1.pop();
+// console.log(myArry1);
+
+// myArray.unshift(0);
+// console.table(myArray);
+
+// marverlHeroes.shift(0);
+// console.table(marverlHeroes);
+
+
+// console.log(myArray.includes(2));
+// console.log(myArray.includes("Chandu"));
+// console.log(myArray.indexOf(2));
+
+// const newArr = myArray.join();
+// console.log(newArr);
+// console.log(myArray);
+// console.log(typeof newArr);
+
+// slice , splice
+
+// console.log("A",myArray);
+
+// const myNewArr1 = myArray.slice(1,3);
+// console.log(myNewArr1);
+
+// const myn1 = myArray.slice(1,3);
+
+// console.log(myn1);
+// console.log("B",myArray);
+
+
+// const myn2 = myArray.splice(1,3);
+// console.log("C", myn2);
+// console.log("D",myArray);
+
+
+
