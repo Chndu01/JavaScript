aee2a4e1fd631c9de17ecc615c2233c57cb14c4b
Chndu01 <contactchandrakantt@gmail.com>
2025-03-15 13:09:42 +0530

    Date and Time

--- 01_Basics/7_date_and_time.js
+++ 01_Basics/7_date_and_time.js
@@ -1,21 +1,21 @@
 // Date and Time
 
-// let myDate = new Date();
-// console.log(myDate.toString());
-// console.log(myDate.toDateString());
-// console.log(myDate.toLocaleString());
-// console.log(myDate.toISOString());
-// console.log(typeof myDate);
+let myDate = new Date();
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toISOString());
+console.log(typeof myDate);
 
 // let myCreatedDate = new Date(2023, 0, 23);
 // let myCreatedDate = new Date(2023, 0, 23, 5, 3);
 // console.log(myCreatedDate.toLocaleString());
 
-let myCreatedDate = new Date("01-12-2003");
+// let myCreatedDate = new Date("01-12-2003");
 
 let myTimeStamp = Date.now();
-// console.log(myTimeStamp);
-// console.log(myCreatedDate.getTime())
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime())
 
 let newDate = new Date();
 console.log(newDate);
