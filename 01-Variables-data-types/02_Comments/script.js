fb19b7f98e9c008b410d7119929414d9c0c57df1
Chndu01 <contactchandrakantt@gmail.com>
2025-08-30 08:27:15 +0530

    Comments

---
+++ 01-Variables-data-types/02_Comments/script.js
@@ -0,0 +1,15 @@
+// This is a Single Line Commnet
+
+/*
+This is a Multi-Line Comment
+const x = 100;
+console.log(x);
+console.log("Hello",100,true);
+*/
+
+console.log("Hello");
+
+console.log(100);
+
+console.log(true);
+
